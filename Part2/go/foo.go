fba455d6ee449b2ff76900d629213787776a7da9
Andreas Aurlien <andraurl@stud.ntnu.no>
2019-01-21 14:28:36 +0100

    Implemented go code.

--- Part2/go/foo.go
+++ Part2/go/foo.go
@@ -3,8 +3,8 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
+	. "fmt"
+	"runtime"
 )
 
 // Control signals
@@ -13,30 +13,43 @@ const (
 	Exit
 )
 
-func number_server(add_number <-chan int, control <-chan int, number chan<- int) {
+func numbeServer(addNumber <-chan int, control <-chan int, number chan<- int) {
 	var i = 0
 
 	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 	for {
 		select {
+		case update := <-addNumber:
+			i += update
+
+		case signal := <-control:
+			if signal == GetNumber {
+				number <- i
+			}
+			if signal == Exit {
+				return
+			}
+
 			// TODO: receive different messages and handle them correctly
 			// You will at least need to update the number and handle control signals.
 		}
 	}
 }
 
-func incrementing(add_number chan<-int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
-		add_number <- 1
+func incrementing(addNumber chan<- int, finished chan<- bool) {
+	for j := 0; j < 1000000; j++ {
+		addNumber <- 1
 	}
 	//TODO: signal that the goroutine is finished
+	finished <- true
 }
 
-func decrementing(add_number chan<- int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
-		add_number <- -1
+func decrementing(addNumber chan<- int, finished chan<- bool) {
+	for j := 0; j < 1000000-1; j++ {
+		addNumber <- -1
 	}
 	//TODO: signal that the goroutine is finished
+	finished <- true
 }
 
 func main() {
@@ -44,12 +57,22 @@ func main() {
 
 	// TODO: Construct the required channels
 	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
+	finished := make(chan bool, 2)
+	number := make(chan int)
+	addNumber := make(chan int)
+	control := make(chan int)
 
 	// TODO: Spawn the required goroutines
+	go numbeServer(addNumber, control, number)
+	go incrementing(addNumber, finished)
+	go decrementing(addNumber, finished)
+
+	<-finished
+	<-finished
 
 	// TODO: block on finished from both "worker" goroutines
 
-	control<-GetNumber
-	Println("The magic number is:", <- number)
-	control<-Exit
+	control <- GetNumber
+	Println("The magic number is:", <-number)
+	control <- Exit
 }
